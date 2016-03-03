2cc821721228f83b43327ac8da4bc073f6b5bfb0
iganbold <itgel.ganbold1@gmail.com>
2016-03-03 23:26:28 +0000

    Learn var getter and setter jsGetSet.js

---
+++ jsOOP/jsGetSet.js
@@ -0,0 +1,36 @@
+// var address = {
+//     street: 'No street',
+//     state: 'No state',
+    
+//     get getStreet() {
+//         return this.street;
+//     },
+    
+//     get getState() {
+//         return this.state;
+//     },
+    
+//     set setStreet(street) {
+//         this.street = street || 'no street';
+//     },
+    
+//     set setState(state) {
+//         this.state = state || 'no state';
+//     },
+    
+//     toString() {
+//         return this.street +", "+ this.state;
+//     }
+// }
+
+// console.log(address.toString());
+
+// address.setStreet = "main street";
+// address.setState = null; 
+
+// console.log(address.toString());
+
+// console.log(address.getState+" "+address.getStreet);
+
+
+// Create getter and setter with Constructor
