26836d5d351100fcbaaf8719c390c99e86a9d9c6
iganbold <itgel.ganbold1@gmail.com>
2016-03-03 23:45:26 +0000

    Learn Object.__defineGet or Set jsGetSet.js

--- jsOOP/jsGetSet.js
+++ jsOOP/jsGetSet.js
@@ -33,4 +33,20 @@
 // console.log(address.getState+" "+address.getStreet);
 
 
-// Create getter and setter with Constructor
+// Create getter and setter with Object.__define__.call()
+
+function Coordinats() {
+    this.lat = 'No lat';
+    this.lon = 'not lon';
+}
+
+Object.__defineGetter__.call(Coordinats.prototype,"getCoords", function(){ return this.lat + " " + this.lon});
+Object.__defineSetter__.call(Coordinats.prototype,"setCoords", function(string) { 
+    var parts = string.split(", ");
+    this.lat = parts[0] || "00";
+    this.lon = parts[1] || "00";
+});
+
+var coords = new Coordinats();
+coords.setCoords = "12, 15";
+console.log(coords.getCoords);
