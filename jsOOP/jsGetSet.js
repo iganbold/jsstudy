07babe6b1c8f70e9756bcc3f824c619a344f9492
iganbold <itgel.ganbold1@gmail.com>
2016-03-03 23:58:27 +0000

    Learn Object.definePropertye jsGetSet.js

--- jsOOP/jsGetSet.js
+++ jsOOP/jsGetSet.js
@@ -32,21 +32,52 @@
 
 // console.log(address.getState+" "+address.getStreet);
 
-
+// *************************************
 // Create getter and setter with Object.__define__.call()
 
-function Coordinats() {
-    this.lat = 'No lat';
-    this.lon = 'not lon';
+// function Coordinats() {
+//     this.lat = 'No lat';
+//     this.lon = 'not lon';
+// }
+
+// Object.__defineGetter__.call(Coordinats.prototype,"getCoords", function(){ return this.lat + " " + this.lon});
+// Object.__defineSetter__.call(Coordinats.prototype,"setCoords", function(string) { 
+//     var parts = string.split(", ");
+//     this.lat = parts[0] || "00";
+//     this.lon = parts[1] || "00";
+// });
+
+// var coords = new Coordinats();
+// coords.setCoords = "12, 15";
+// console.log(coords.getCoords);
+
+// **************************************
+// Create getter and setter with Object.defineProperty()
+
+function Point() {
+    this.xPos = 0;
+    this.yPos = 0;
 }
 
-Object.__defineGetter__.call(Coordinats.prototype,"getCoords", function(){ return this.lat + " " + this.lon});
-Object.__defineSetter__.call(Coordinats.prototype,"setCoords", function(string) { 
-    var parts = string.split(", ");
-    this.lat = parts[0] || "00";
-    this.lon = parts[1] || "00";
+Object.defineProperty(Point.prototype, "pointPos" , {
+    get() {
+      return "Positions: "+ this.xPos + ", "+ this.yPos;   
+    },
+    set(string) {
+        var parts = string.split(", ");
+        this.xPos = parts[0] || "00";
+        this.yPos = parts[1] || "00";
+    }
 });
 
-var coords = new Coordinats();
-coords.setCoords = "12, 15";
-console.log(coords.getCoords);
+var p1 = new Point();
+
+p1.pointPos = "200, 400";
+
+console.log(p1.pointPos);
+
+
+
+
+
+
